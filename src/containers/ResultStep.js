335a737b9c8d09d602eab65a7b683c04feb0ef7f
sshpro22 <sshpro22@gmail.com>
2019-07-25 18:10:34 -0400

    fixed app crash, added prop type checking

--- src/containers/ResultStep.js
+++ src/containers/ResultStep.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React, { useEffect } from "react"
+import PropTypes from "prop-types"
 import { Button } from 'react-bootstrap'
 
 // This is step 3(result step) in the wizard form
 // Calc the rate from the selected data and show the result
 
 const ResultStep = ({ selectedOption, onBack, rates }) => {
+
 
     // Get and calc how much 1 BTC is in the selected currency
     const selectedRate = rates[selectedOption];
@@ -14,14 +16,21 @@ const ResultStep = ({ selectedOption, onBack, rates }) => {
     
     // detect the minimize, reopen event and 
     // take person to the second(currency) screen
-
-    document.addEventListener("visibilitychange", handleVisibilityChange, false);
-    const handleVisibilityChange = () => {
-        if (document.hidden === false) {
-            document.removeEventListener("visibilitychange", handleVisibilityChange);
-            onBack()
+    // corresponds to componentDidMount/componentWillUnmount
+    useEffect(() => {
+        const handleVisibilityChange = () => {
+            if (document.hidden === false) {
+                onBack();
+            }
         }
-    }
+
+        document.addEventListener("visibilitychange", handleVisibilityChange, false);
+
+        return () => {
+            document.removeEventListener("visibilitychange", handleVisibilityChange);
+        }
+    }, [onBack]);
+
 
     return (
         <div>
@@ -31,6 +40,12 @@ const ResultStep = ({ selectedOption, onBack, rates }) => {
             <Button onClick={onBack}>Choose Currency</Button>
         </div>
     )
+}
+
+ResultStep.propTypes = {
+    selectedOption: PropTypes.string.isRequired,
+    onBack: PropTypes.func.isRequired,
+    rates: PropTypes.object,
 }
 
 export default ResultStep
